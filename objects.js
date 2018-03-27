f7de6e47d66757acac71781326e9577fb45aaa62
grant diamond <grant.a.diamond@gmail.com>
2018-03-27 15:07:57 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,7 +1,7 @@
 var playlist = {'Slowdive':'Alison', 'My Bloody Valentine':'Sometimes'}
 
-function updatePlaylist(playlist, artistName, songTitle){
-  playlist[artistName]=songTitle
+function updatePlaylist(playlist, artist, song){
+  playlist[artist]=song
   return playlist
 }
 
