fbb18586fe490384463d5fbe513e7eec2147239a
grant diamond <grant.a.diamond@gmail.com>
2018-03-27 13:10:42 +0000

    automatically backed up by learn

---
+++ objects.js
@@ -0,0 +1,12 @@
+var playlist = {theBeatles: "can't buy me love", theRollingStones: "paint in black", theEagles: "hotel california"}
+
+function updatePlaylist(playlist, artist, title){
+
+Object.assign( {}, playlist, {[artist]: title)})
+  
+}
+
+function removeFromPlaylist(playlist, artist){
+  delete playlist.artist
+  return playlist
+}
